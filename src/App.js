96ed22aa8cf1cb58803ad18199a448ab16e5ff61
ThanhLD <duythanhcn@gmail.com>
2022-03-24 23:03:39 +0700

    add capture screen

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Fragment, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ScreenCapture from './ScreenCapture';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const link = 'http://giaothong.hochiminhcity.gov.vn:8007/Render/CameraHandler.ashx?id=5d9ddf49766c880017188ca0&bg=black&w=300&h=230&t=1583674013'
+class App extends Component {
+  state = {
+    name: 'GAMO',
+    screenCapture: ''
+  }
+
+  handleScreenCapture = (screenCapture) => {
+    this.setState({
+      screenCapture
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <ScreenCapture onEndCapture={this.handleScreenCapture}>
+          {({ onStartCapture }) => (
+            <Fragment>
+              <img src={link} />
+              <button onClick={onStartCapture}>Capture</button>
+            </Fragment>
+          )}
+        </ScreenCapture>
+        <div className='img-ref'>
+          <img src={this.state.screenCapture} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
